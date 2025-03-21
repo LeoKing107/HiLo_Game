3c3e630e48be3ef8136c747243d79bb7b4a973df
LeoKing107 <lalasimba764@gmail.com>
2025-03-20 19:17:30 -0700

    Program

--- obj/Debug/net8.0/HiLoGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/HiLoGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HiLoGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5537ae7cecdbf5de4a354dd1d7aae5c9d4b02f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22162b366259f2320319e4dca83e68027105134a")]
 [assembly: System.Reflection.AssemblyProductAttribute("HiLoGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HiLoGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
